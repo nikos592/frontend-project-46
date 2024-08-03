a39b073de4870a53acc85ef36200835fad80bdae
Илья Никулин <wixestrees@gmail.com>
2024-08-03 14:01:07 +0300

    corrected step 3

--- src/gendiff.js
+++ src/gendiff.js
@@ -2,6 +2,7 @@
 
 const { Command } = require('commander');
 const { version } = require('../package.json');
+const { generateDiff } = require('./generateDiff.js');
 
 const program = new Command();
 
@@ -9,8 +10,23 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference')
   .version(version, '-V, --version', 'output the version number')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format') 
   .helpOption('-h, --help', 'output usage information')
   .usage('[options] <filepath1> <filepath2>');
 
 program.parse(process.argv);
+
+const options = program.opts();
+const [filepath1, filepath2] = program.args;
+
+if (!filepath1 || !filepath2) {
+  console.error('Please provide filepath1 and filepath2');
+  process.exit(1);
+}
+
+try {
+  generateDiff(filepath1, filepath2);
+} catch (error) {
+  console.error('Error comparing files:', error.message);
+  process.exit(1);
+}
--- src/generateDiff.js
+++ src/generateDiff.js
@@ -24,23 +24,27 @@ const parseFile = (filePath) => {
 const buildDiff = (obj1, obj2, depth = 1) => {
     const allKeys = _.union(_.keys(obj1), _.keys(obj2)).sort();
     const indent = ' '.repeat(depth * 4); 
-
     const lines = allKeys.map((key) => {
         const value1 = obj1[key];
         const value2 = obj2[key];
 
         if (!_.has(obj1, key)) {
+            
             return `${indent}+ ${key}: ${isObject(value2) ? buildDiff(value2, value2, depth + 1) : value2}`;
         } else if (!_.has(obj2, key)) {
+            
             return `${indent}- ${key}: ${isObject(value1) ? buildDiff(value1, value1, depth + 1) : value1}`;
         } else if (_.isObject(value1) && _.isObject(value2)) {
+            
             return `${indent}  ${key}: ${buildDiff(value1, value2, depth + 1)}`;
         } else if (!_.isEqual(value1, value2)) {
+            
             return [
                 `${indent}- ${key}: ${isObject(value1) ? buildDiff(value1, value1, depth + 1) : value1}`,
                 `${indent}+ ${key}: ${isObject(value2) ? buildDiff(value2, value2, depth + 1) : value2}`
             ].join('\n');
         } else {
+            
             return `${indent}  ${key}: ${value1}`;
         }
     });
